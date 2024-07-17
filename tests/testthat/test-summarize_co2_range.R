930a3bc59ae9cff6d3f7e7a1fbb0bf4165f2b090
Mauro Lepore <maurolepore@gmail.com>
2024-07-17 08:12:15 -0300

    Polish DESCRIPTION (#6)
    * Polish DESCRIPTION
    
    * * chore: Test crucial columns (#7)

--- tests/testthat/test-summarize_co2_range.R
+++ tests/testthat/test-summarize_co2_range.R
@@ -9,3 +9,25 @@ test_that("yields the expected rows", {
   out <- summarize_co2_range(data)
   expect_equal(unique(out$benchmark), col_pick())
 })
+
+test_that("errors gracefully", {
+  data <- emissions_product
+
+  col_rm <- function(data, crucial) data[setdiff(names(data), crucial)]
+
+  crucial <- col_by()
+  bad <- col_rm(data, crucial)
+  expect_error(summarize_co2_range(bad), crucial)
+
+  crucial <- col_pick()
+  bad <- col_rm(data, crucial)
+  expect_error(summarize_co2_range(bad), crucial)
+
+  crucial <- col_min()
+  bad <- col_rm(data, crucial)
+  expect_error(summarize_co2_range(bad), crucial)
+
+  crucial <- col_max()
+  bad <- col_rm(data, crucial)
+  expect_error(summarize_co2_range(bad), crucial)
+})
