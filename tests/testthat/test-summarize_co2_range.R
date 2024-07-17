137e91a8628d754adb228e10d0b46aef9299659e
Mauro Lepore <maurolepore@gmail.com>
2024-07-17 00:09:30 +0000

    Make tests more maintainable

--- tests/testthat/test-summarize_co2_range.R
+++ tests/testthat/test-summarize_co2_range.R
@@ -1,5 +1,11 @@
-test_that("yields the expected data structure", {
+test_that("yields the expected columns", {
+  data <- emissions_product
+  crucial <- col_summarize_co2_range()
+  expect_named(summarize_co2_range(data), crucial)
+})
+
+test_that("yields the expected rows", {
   data <- emissions_product
   out <- summarize_co2_range(data)
-  expect_snapshot(str(out))
+  expect_equal(unique(out$benchmark), col_pick())
 })
