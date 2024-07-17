e59fc440b9aa1a4e525bfc7e2d9b30c887b15759
Mauro Lepore <maurolepore@gmail.com>
2024-07-16 21:40:59 -0300

    Export (#5)
    * Export
    
    * Update README

--- R/summarize_co2_range.R
+++ R/summarize_co2_range.R
@@ -1,3 +1,13 @@
+#' Summarize the range of CO2
+#'
+#' @param data Data frame. The product level result of the emissions indicator.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' data <- emissions_product
+#' summarize_co2_range(data)
 summarize_co2_range <- function(data) {
   cols <- c(col_by(), col_pick(), col_min(), col_max())
   rows <- data[[col_by()]] == col_pick()
