37b76f572077cfd8ebe0b14d72b8c37eecc7e563
Mauro Lepore <maurolepore@gmail.com>
2024-07-17 11:39:07 +0000

    Check crucial names

--- R/summarize_co2_range.R
+++ R/summarize_co2_range.R
@@ -10,6 +10,7 @@
 #' summarize_co2_range(data)
 summarize_co2_range <- function(data) {
   cols <- c(col_by(), col_pick(), col_min(), col_max())
+  check_crucial_names(data, cols)
   rows <- data[[col_by()]] == col_pick()
   data[rows, cols, drop = FALSE]
 }
